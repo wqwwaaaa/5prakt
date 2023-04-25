3b5a2ac8467f7c627a4e4d7ee6dc3b49908d2756
wqwwaaaa <131134853+wqwwaaaa@users.noreply.github.com>
2023-04-25 10:28:59 +0300

    Add files via upload

---
+++ WpfApp72(2)/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp72
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int k = -2;
+                int n = Convert.ToInt32(ListBoxData.Items[0]);
+                int max = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    int m = Convert.ToInt32(ListBoxData.Items[i]);
+                    if (m % 10 == 3)
+                    {
+                        if (k == -2)
+                        {
+                            max = m;
+                            k = 0;
+                        }
+                        if (max < m)
+                        {
+                            max = m;
+                        }
+                    }
+                }
+                if (k != -2)
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n{max}";
+
+                }
+                else
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n Чисел оканчивающихся на 3 нет";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TbNumber.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TbNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add(TbNumber.Text);
+        }
+    }
+}
