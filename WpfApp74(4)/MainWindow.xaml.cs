42097d7f66a1543a2db9e4d31272983d9629c632
wqwwaaaa <131134853+wqwwaaaa@users.noreply.github.com>
2023-04-25 10:32:00 +0300

    Add files via upload

---
+++ WpfApp74(4)/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp74
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int k = -1;
+                double A = Convert.ToDouble(TbNumberA.Text);
+                int N = Convert.ToInt32(TbNumberB.Text);
+                double r = 0;
+                for (int i = 0; i <= N; i++)
+                {
+                    if (k == -1)
+                    {
+                        double el = Math.Pow(A, i);
+                        if (i % 2 == 1)
+                        {
+                            el = -el;
+                            ListBoxData.Items.Add(el.ToString());
+                        }
+                        r += el;
+                    }
+                }
+                if (k == -1)
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n{r}";
+                }
+                else
+                {
+                    TextBlockAnswer.Text = "Неверные значения";
+                }
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ WpfApp75(5)/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp75
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int k = -1;
+                double xn, yn;
+                double sum = 0;
+                double xn_1 = 1;
+                double yn_1 = 1;
+                double n = Convert.ToDouble(TbNumberA.Text);
+                
+                if (k == -1)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        xn = xn_1 * 0.3;
+                        yn = xn_1 + yn_1;
+                        double an = xn / (1 + Math.Abs(yn));
+                        ListBoxData.Items.Add(an.ToString("F5"));
+                        sum += an;
+                        xn_1 = xn;
+                        yn_1 = yn;
+
+                    }
+                }
+                if (k == -1)
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n{sum}";
+                }
+                else
+                {
+                    TextBlockAnswer.Text = "Неверные значения";
+                }
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
